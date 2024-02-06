587571eccd681774fbe79c6c7975f514515c7509
Talukder-Asif <asif.talukder.aj.10@gmail.com>
2024-02-06 22:31:16 +0600

    Mongodb setup done

--- index.js
+++ index.js
@@ -11,6 +11,51 @@ app.use(cors());
 app.use(express.json());
 
 
+// MongoDB
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.PASSWORD}@cluster0.eykzqz7.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    // await client.connect();
+    const database = client.db("RealEstate");
+    const userCollection = database.collection("UserData");
+
+
+
+
+
+
+    
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+
+
+
+
 // Route
 app.get('/', (req, res) => {
     res.send('Hello World!');
